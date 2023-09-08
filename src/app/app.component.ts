1cef6847932adf9e61cf0380e3a6387fb8979efd
Jorge Guepud <jlguepudb@unal.edu.co>
2023-09-08 01:40:20 -0500

    feat: se tiene una versión jugable, pero, algo no funciona bien

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Board } from './models/board.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'game-of-life';
+export class AppComponent implements OnInit {
+  public title: string = 'Game Of Life';
+
+  public numCols: number;
+  public numRows: number;
+  public generation: number;
+  public gameStatus: number; // -1: no empieza | 0: Activo | 1: Pausado
+  
+  public arrNumCols: number[];
+  public arrNumRows: number[];
+
+  public board: Board;
+
+  constructor(){
+    this.numCols = 40;
+    this.numRows = 40;
+    this.generation = 0;
+    this.gameStatus = 0;
+
+    this.arrNumCols = Array.from({length: this.numCols}, (v,k)=> k);
+    this.arrNumRows = Array.from({length: this.numRows}, (v,k)=> k);
+
+    this.board = new Board(this.numCols, this.numRows);
+  }
+
+  ngOnInit(): void {
+    
+    setInterval(() => {
+      if(this.gameStatus === 0){
+        this.board.checkBoard();
+        this.generation++;
+      }
+    }, 5000);
+    
+  }
+
+  onClick(pRow: number, pCol: number){
+    this.board.changeStatus(pRow,pCol);
+  }
+  
 }
---
+++ src/app/models/board.model.ts
@@ -0,0 +1,78 @@
+export class Board{
+
+    private board: number[][];
+
+    constructor(pWidth: number, pHeight: number){
+
+        this.board = [];
+
+        this.fillBoard(pWidth, pHeight);
+        
+    }
+
+    private fillBoard(pWidth: number, pHeight: number): void {
+        for (let c = 0; c < pWidth; c++) {
+            this.board[c] = [];
+            for (let f = 0; f < pHeight; f++) {
+                this.board[c][f] = 0;          
+            }
+        }
+    }
+
+    public status(coordX: number, coordY: number): number {
+        return this.board[coordX][coordY];
+    }
+
+    public changeStatus(coordX: number, coordY: number): void {
+        this.board[coordX][coordY] = this.board[coordX][coordY] === 0 ? 1 : 0;
+    }
+
+    public checkBoard(): void {
+
+        let tmpBoard: number[][] = [];
+
+        for (let c = 0; c < this.board.length; c++) {
+            tmpBoard[c] = [];
+            for (let f = 0; f < this.board.length; f++) {
+                tmpBoard[c].push(this.checkRules(c,f));
+                
+            }
+        }
+
+        this.board = [...tmpBoard];
+    }
+
+    public checkRules(coordX: number, coordY: number): number {
+
+        const width: number = this.board.length;
+        const height: number = this.board[0].length;
+
+        const xMenos: number = (coordX - 1 < 0) ? (width - 1) : (coordX - 1);
+        const xMas: number = (coordX + 1 >= width) ? 0 : (coordX + 1);
+
+        const yMenos: number = (coordX - 1 < 0) ? (height - 1) : (coordY - 1);
+        const yMas: number = (coordX + 1 >= height) ? 0 : (coordX + 1);
+
+        const currentStatus: number = this.board[coordX][coordY];
+
+        const vecinos: number = 
+            this.board[xMenos][yMenos]  +
+            this.board[xMenos][coordY]  +
+            this.board[xMenos][yMas]    +
+            this.board[coordX][yMenos]  +
+            this.board[coordX][yMas]    +
+            this.board[xMas][yMenos]    +
+            this.board[xMas][coordY]    +
+            this.board[xMas][yMas];
+
+        if(currentStatus === 1 && (vecinos === 2 || vecinos === 3)) {
+            return 1;
+        }
+
+        if(currentStatus === 0 && vecinos === 3){
+            return 1;
+        }
+
+        return 0;
+    }
+}
