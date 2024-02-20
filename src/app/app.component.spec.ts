d64ce9cd1326282aadb3660512c1568146629a73
Jorge Guepud <jlguepudb@unal.edu.co>
2024-02-20 06:33:02 -0500

    feat: se agregan archivos para github actions

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'game-of-life'`, () => {
+  it(`should have as title 'Game Of Life'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('game-of-life');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('game-of-life app is running!');
+    expect(app.title).toEqual('Game Of Life');
   });
 });
